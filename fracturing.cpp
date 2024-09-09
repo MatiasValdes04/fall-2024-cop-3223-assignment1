5c982d8270a24aec242760bdb0d43b67c74d21f8
MatiasValdes04 <matiasvaldei@gmail.com>
2024-09-09 01:04:43 +0000

    9pm check

--- fracturing.cpp
+++ fracturing.cpp
@@ -1,4 +1,5 @@
-//headers
+//5534049
+//Matias Valdes
 #include <stdio.h>
 #include <math.h>
 
@@ -31,7 +32,7 @@ double askForUserInput() {
     return value;
 }
 
-//calculating distance, perimeter, area, width, height
+//calculating distance
 double calculateDistance() {
     double x1 = askForUserInput();
     double y1 = askForUserInput();
@@ -40,41 +41,40 @@ double calculateDistance() {
 
     printf("Point #1 entered: x1 = %lf, y1 = %lf\n", x1, y1);
     printf("Point #2 entered: x2 = %lf, y2 = %lf\n", x2, y2);
-
+//double representing the distance
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     printf("The distance between the two points is %lf\n", distance);
-
     return distance;
 }
-
+//calculating perimeter
 double calculatePerimeter() {
     double distance = calculateDistance();
     double perimeter = 2 * PI * distance;
     printf("The perimeter of the circle is %lf\n", perimeter);
 
-    return perimeter;
+    return 2;
 }
-
+//calculating area
 double calculateArea() {
     double distance = calculateDistance();
     double area = PI * pow(distance, 2);
     printf("The area of the circle is %lf\n", area);
-
-    return area;
+//had to figure out how to do powers
+    return 2;
 }
-
+//calculating width
 double calculateWidth() {
     double distance = calculateDistance();
     double width = 2 * distance;
     printf("The width of the circle is %lf\n", width);
 
-    return width;
+    return 1;
 }
-
+//calculating height
 double calculateHeight() {
     double distance = calculateDistance();
     double height = 2 * distance;
     printf("The height of the circle is %lf\n", height);
 
-    return height;
+    return 1;
 }
